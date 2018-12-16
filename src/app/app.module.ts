8e29769fb24954cc151849af7a6555af90e0aac2
Your Name <satishchemudu@gmail.com>
2018-12-16 16:31:20 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{  Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { Test2Component } from './test2/test2.component';
+import { TestCompComponent } from './test-comp/test-comp.component';
+
+
+
+const routes  : Routes=[
+  { path :'Test', component: TestCompComponent},
+  { path :'', component: TestComponent},
+  { path :'*', component: TestComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    Test2Component
+    Test2Component,
+    TestCompComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot
+    (
+      routes,  { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-test',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _route : Router) { }
 
   ngOnInit() {
+  }
+
+
+  Link()
+  {
+    this._route.navigate(['Test']);
   }
 
 }
